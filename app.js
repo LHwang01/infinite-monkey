9c05ada2c5e06c68c4503a07836fee56f2e2eeb7
LHwang01 <LawrenceHwang01@gmail.com>
2022-05-08 21:57:29 -0400

    Added Code to Generate Random String and Compare
    Need to fix browser freezing issue

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz ";
+
 function getText() {
-    var text = document.getElementById('input');
-    alert(text.value);
+    setInterval(monkeyStart, 10);
+}
+
+function monkeyStart() {
+    let text = document.getElementById('input').value.toLowerCase();
+    let generatedString = generateWord(text);
+    
+    while (generatedString != text) {
+        generatedString = "";
+
+        generatedString = generateWord(text);
+    }
+
+    alert("Exited!");
+}
+
+function generateWord(text) {
+    let generatedWord = "";
+
+    for (let i = 0; i < text.length; i++) {
+        let randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+        generatedWord += randomCharacter;
+    }
+
+    return generatedWord;
 }
