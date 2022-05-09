3c343843bed19517e8e4e6f2e13dbb2285a95a85
LHwang01 <LawrenceHwang01@gmail.com>
2022-05-08 22:39:32 -0400

    Re-Formated Code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const worker = new Worker("compute.js");
 
-function monkeyStart() {
+function monkeyStart() {    
     let text = document.getElementById('input').value.toLowerCase();
+
     worker.postMessage(text);
 }
--- compute.js
+++ compute.js
@@ -1,13 +1,13 @@
 const alphabet = "abcdefghijklmnopqrstuvwxyz ";
 
-onmessage = function(message)
-{
-    let generatedString = generateWord(message.data);
-    
-    while (generatedString != message.data) {
+onmessage = function (message) {
+    let messageValue = message.data;
+    let generatedString = generateWord(messageValue);
+
+    while (generatedString != messageValue) {
         generatedString = "";
 
-        generatedString = generateWord(message.data);
+        generatedString = generateWord(messageValue);
     }
 
     console.log("Exited!");
